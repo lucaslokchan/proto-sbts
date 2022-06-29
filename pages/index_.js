a7652b2ab6c03ab62daaf11bbf9b0532487d58b3
lucaslokchan <lucas.lokchan@gmail.com>
2022-06-29 23:34:17 +0800

    Update index_.js

--- pages/index_.js
+++ pages/index_.js
@@ -2,17 +2,23 @@ import Link from 'next/link'
 
 export default function Home() {
   return (
-   <><div className="text-center align-middle">
+  <>
+  <div className='space-y-96'>
+    <div>
       <h1 className="text-9xl">SOULBOUND</h1>
     </div>
     <div>
-        <a className="text-5xl" href="https://metamask.io/download/" target="_blank">Install Metamask</a>
-        <h1 className="text-5xl">Add Network</h1>
-        <Link href="/"><a className="text-5xl" >Get Started</a></Link>
+      <a className="text-8xl" href="https://metamask.io/download/" target="_blank">Install Metamask</a>
     </div>
-      
-      
-      </>
-   
+    <div>
+      <button className="text-left text-8xl">Add Network</button>
+    </div>
+    <div>
+      <Link href="/"><a className="text-8xl" >Get Started</a></Link>
+    </div>
+    <div>
+    </div>
+  </div>
+  </>
   )
 }
