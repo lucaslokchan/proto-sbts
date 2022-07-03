f3a281a544b097d617f8532e15eea5b8e9a4eb27
lucaslokchan <lucas.lokchan@gmail.com>
2022-07-03 22:47:21 +0800

    Update cards css layout

--- pages/demo.js
+++ pages/demo.js
@@ -4,20 +4,111 @@ import Link from 'next/link'
 export default function Home() {
   return (
   <>
-    <div class="max-w-sm rounded overflow-hidden shadow-lg">
-  <img class="w-full" src="/img/card-top.jpg" alt="Sunset in the mountains"/>
-  <div class="px-6 py-4">
-    <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
-    <p class="text-gray-700 text-base">
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
-    </p>
-  </div>
-  <div class="px-6 pt-4 pb-2">
-    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
-    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
-    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
-  </div>
-</div>
-  </>
+  <div className="border-1">
+                <div className='grid grid-cols-1 md:grid-cols-2'>
+                    <div>
+                        <h1>
+                            Lucas
+                        </h1>
+                        <p>
+                        Lucas graduated from Lumbburgh University with an Electrical and Electronics Degree. He works as a data scientist and part time research assistant at Lumbburgh University electronic lab. He recently attended blockchain workshop. On the personal side, he is a gold member at ??? club
+                        </p>
+                    </div>
+
+                    <div className="h-fit">
+                        <h1 className="title is-2">
+                            Stats
+                        </h1>
+                        <h2 className="subtitle is-4">
+                            SBTs Owned: 
+                        </h2>
+                        <h2 className="subtitle is-4">
+                            Total Supply:
+                        </h2>
+                    </div>
+                </div>
+            </div>
+
+
+            <div className="flex justify-center">
+              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 md:gap-28 ">
+                
+                <div className="w-[500px] h-[550px] max-w-xl overflow-hidden align-top border-2 rounded shadow-lg hover:bg-red-400">
+                  <div className="grid place-items-center">
+                      <img className="object-center w-2/5"/>
+                  </div>
+                  &nbsp;
+                  <div>
+                      <div className="grid place-items-center">
+                          <p className="mb-2 text-xl font-bold place-content-center"></p>
+                      </div>
+                      <p>123</p>
+                      <p>123</p>
+                      <p>123</p>
+                  </div>
+                </div>     
+
+                <div className="w-[500px] h-[550px] overflow-hidden align-top border-2 rounded shadow-lg hover:bg-red-400">
+                  <div className="grid place-items-center">
+                      <img className="object-center w-2/5"/>
+                  </div>
+                  &nbsp;
+                  <div>
+                      <div className="grid place-items-center">
+                          <p className="mb-2 text-xl font-bold place-content-center"></p>
+                      </div>
+                      <p>123</p>
+                      <p>123</p>
+                      <p>123</p>
+                  </div>
+                </div> 
+
+                <div className="w-[500px] h-[550px] overflow-hidden align-top border-2 rounded shadow-lg hover:bg-red-400">
+                  <div className="grid place-items-center">
+                      <img className="object-center w-2/5"/>
+                  </div>
+                  &nbsp;
+                  <div>
+                      <div className="grid place-items-center">
+                          <p className="mb-2 text-xl font-bold place-content-center"></p>
+                      </div>
+                      <p>123</p>
+                      <p>123</p>
+                      <p>123</p>
+                  </div>
+                </div> 
+
+                <div className="w-[500px] h-[550px] overflow-hidden align-top border-2 rounded shadow-lg hover:bg-red-400">
+                  <div className="grid place-items-center">
+                      <img className="object-center w-2/5"/>
+                  </div>
+                  &nbsp;
+                  <div>
+                      <div className="grid place-items-center">
+                          <p className="mb-2 text-xl font-bold place-content-center"></p>
+                      </div>
+                      <p>123</p>
+                      <p>123</p>
+                      <p>123</p>
+                  </div>
+                </div> 
+
+                <div className="w-[500px] h-[550px] overflow-hidden align-top border-2 rounded shadow-lg hover:bg-red-400">
+                  <div className="grid place-items-center">
+                      <img className="object-center w-2/5"/>
+                  </div>
+                  &nbsp;
+                  <div>
+                      <div className="grid place-items-center">
+                          <p className="mb-2 text-xl font-bold place-content-center"></p>
+                      </div>
+                      <p>123</p>
+                      <p>123</p>
+                      <p>123</p>
+                  </div>
+                </div> 
+              </div>
+            </div>
+      </>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,241 +1,66 @@
-import Head from 'next/head'
+import Link from 'next/link'
 
-import styles from '../styles/SBT.module.css'
-import Web3 from 'web3'
-import { useState, useEffect } from 'react'
-import sbtContract from '../sbt'
-import cards from '../styles/CARDS.module.css'
-
-const Sbt = () => {
-    //state hook for storing error message in error variable
-    const [error, setError] = useState('')
-    const [address, setAddress] = useState('')
-    const [uri, setURI] = useState('')
-    const [desc, setDesc] = useState('')
-    const [image, setImage] = useState('')
-    const [tokenowned, setTokenOwned] = useState([])
-    const [buttontext, setButtonText] = useState('Connect Wallet')
-    const [totalsupply, setTotalSupply] = useState('')
-
-    let url = 'https://ipfs.io/ipfs/' + uri
-
-    //const fetchURI = async () => {
-    //    const response = await fetch(url);
-    //    return response.json();
-    //}
-
-    let web3
-
-    useEffect(() => {
-        getSBTHandler()
-        //fetchURI().then(response => setDesc(response['description']))
-        //connectwalletHandler()
-        //fetchURI().then(response => setDesc(JSON.stringify(response)))
-        //fetchURI().then(response => setImage(response['image']))
-        if (typeof window.ethereum !== 'undefined') {
-            setButtonText(ethereum.selectedAddress)
-        }  
-        getTotalSupplyHandler().then(response => setTotalSupply(response))
-    })
-
-    useEffect(() => {
-        if (typeof window.ethereum !== 'undefined') {
-            if (ethereum.selectedAddress != null) {
-                getTokenOwnedHandler().then(response => setTokenOwned(response)) 
-            }   
-        }
-    }, [])
-
-    //refresh page when wallet address changes
-    useEffect(() => {
-        if (typeof window.ethereum !== 'undefined') {
-            if (window.ethereum) {
-              window.ethereum.on("chainChanged", () => {
-                window.location.reload();
-              });
-              window.ethereum.on("accountsChanged", () => {
-                //window.location.reload();
-                setAddress(ethereum.selectedAddress)
-                getTokenOwnedHandler().then(response => setTokenOwned(response))
-                setButtonText(ethereum.selectedAddress)
-              });
-            }
-            if (ethereum.selectedAddress == null) {
-                setButtonText("Connect Wallet")
-            }
-        }
-      });
-    
-    const getSBTHandler = async () => {
-        const uri = await sbtContract.methods.tokenURI(0).call()
-        setURI(uri)
-    }
-
-    const getTotalSupplyHandler = async () => {
-        const supply = await sbtContract.methods.totalSupply().call()
-        return supply
-    }
-
-    const getTokenOwnedHandler = async () => {
-        var tokenid = new Array();
-        var urilist = new Array();
-        var responselist = new Array();
-        const balance = await sbtContract.methods.balanceOf(ethereum.selectedAddress).call((err, result) => { return result });
-        for (let i = 0; i < balance; i++) {
-            let id = await sbtContract.methods.tokenOfOwnerByIndex(ethereum.selectedAddress, i).call()
-            tokenid[i] = id
-        }
-        for (let i = 0; i < tokenid.length; i++) {
-            let uri = await sbtContract.methods.tokenURI(tokenid[i]).call()
-            urilist[i] = uri
-        }
-        for (let i = 0; i < urilist.length; i++) {
-            let response = await fetch(url + urilist[i]).then(data => data.json());
-            responselist.push(response)
-        }
-        return responselist
-    }
-
-    function requestSBTHandler() {
-        sbtContract.methods.requestSBT(ethereum.selectedAddress, "QmUKYnrC1SRdijKpP1hEx4Q3Eon8qF1GQVZ3cktydTM7rW").send({ from: ethereum.selectedAddress })
-    }
-
-    //window.ethereum
-    const connectwalletHandler = async () => {
-        if (typeof window.ethereum !== 'undefined') {
-            try {
-                await window.ethereum.request({ method: 'eth_requestAccounts' });
-                //Instantiate web3 instance for calling smart contract methods
-                web3 = new Web3(window.ethereum)
-                setAddress(ethereum.selectedAddress)
-            } catch(err) {
-                setError(err.message)
-            }
-            
-          } else {
-              //metamask not installed
-              setError("Please install MetaMask")
-          }
-    }
-
-    function dropdownChangeHandler(e) {
-        
-    }
-    return (
-        <><div className={styles.main}>
-            <Head>
-                <title>Soulbound Token</title>
-                <meta name="description" content="Claim you Soulbound Token" />
-            </Head>
-
-            <navbar className="flex justify-end border-2">
-                <button onClick={connectwalletHandler} className="">{buttontext}</button>
-            </navbar>
-
-            <div className="border-1">
-                <div className='grid grid-cols-1 md:grid-cols-2'>
-                    <div>
-                        <h1>
-                            Request Token
-                        </h1>
-                        <select defaultValue={"DEFAULT"} onChange={dropdownChangeHandler}>
-                            <option value="DEFAULT" disabled>Select Token</option>
-                            <option value="1">University Degree</option>
-                            <option value="2">Certificate of Attendance</option>
-                            <option value="3">Membership</option>
-                            <option value="4">Access Right - Property</option>
-                            <option value="5">Access Right - Data Cooperatives</option>
-                            <option value="6">Certificate of Attendance</option>
-                        </select>
-                        <button onClick={requestSBTHandler} className="button">Request SBT</button>
-                        <h1>
-                            Burn Token
-                        </h1>
-                        <select defaultValue={"DEFAULT"} onChange={dropdownChangeHandler}>
-                            <option value="DEFAULT" disabled>Select Token</option>
-                            <option value="1">University Degree</option>
-                            <option value="2">Certificate of Attendance</option>
-                            <option value="3">Membership</option>
-                            <option value="4">Access Right - Property</option>
-                            <option value="5">Access Right - Data Cooperatives</option>
-                            <option value="6">Certificate of Attendance</option>
-                        </select>
-                    </div>
-
-                    <div className="h-fit">
-                        <h1 className="title is-2">
-                            Stats
-                        </h1>
-                        <h2 className="subtitle is-4">
-                            Contract Address: <a href="https://ropsten.etherscan.io/address/0xAab2d8b6F6D3eE17510c87111e1563a4611FfFb2">0xAab2....fFb2</a>
-                        </h2>
-                        <h2 className="subtitle is-4">
-                            SBTs Owned: {tokenowned.length}
-                        </h2>
-                        <h2 className="subtitle is-4">
-                            Total Supply: {totalsupply}
-                        </h2>
-                    </div>
-                </div>
-            </div>
-
-            <section>
-                <div className="container has-text-danger">
-                    <p>{error}</p>
-                </div>
-            </section>
-
-
-            <div className="flex justify-center">
-                <div className="grid grid-cols-1 md:grid-cols-3 md:gap-28">
-                {tokenowned.map(token => {
-                    return (
-                        <div>
-                            {[token].map(tokeninfo => {
-                                return (
-                                    <>
-                                    <div className="max-w-xl align-top border-2 rounded">
-                                        <div className="grid place-items-center">
-                                            <img className="object-center w-2/5" src={tokeninfo.image} />
-                                        </div>
-                                        &nbsp;
-                                        <div>
-                                            <div className="grid place-items-center">
-                                                <p className="mb-2 text-xl font-bold place-content-center">{tokeninfo.title}</p>
-                                            </div>
-                                            <p>{tokeninfo.type}</p>
-                                            <p>{tokeninfo.issuer}</p>
-                                            <p>{tokeninfo.description}</p>
-                                        </div>
-                                    </div>
-                                    </>
-                                )
-                            })}
-                        </div>
-                    )
-                })}
-                </div>
-            </div>
-
-
-
-            <footer className="">
-                <div className="">
-                    <p><strong>© 2022 Lucas Chan</strong> </p>
-                    <p><a href="https://github.com/lucaslokchan/proto-sbts">Github</a> |
-                        <a href="https://www.linkedin.com/in/lucaslokchan/"> Linkedln</a>
-                    </p>
-                </div>
-            </footer>
-        </div>
-
-
-
-
-            </>
-
-      
-        )
+async function addNetwork() {
+  window.ethereum.request({
+    method: "wallet_addEthereumChain",
+    params: [{
+      chainId: '0x89',
+      chainName: 'Matic Mainnet',
+      nativeCurrency: {
+          name: 'MATIC',
+          symbol: 'MATIC',
+          decimals: 18
+      },
+      rpcUrls: ['https://polygon-rpc.com/'],
+      blockExplorerUrls: ['https://polygonscan.com/']
+  }]
+  })
 }
 
-export default Sbt
+export default function Home() {
+  return (
+  <>
+  <div>
+    <div className='space-y-0'>
+      <div>
+        <h1 className="font-bold text-9xl">SOULBOUND</h1>
+      </div>
+      <div>
+        <a className="text-8xl font-semibold hover:opacity-60 transition delay-[60ms]" href="https://metamask.io/download/" target="_blank">Install Metamask</a>
+      </div>
+      <div>
+        <button onClick={addNetwork} className="font-semibold text-left text-8xl hover:opacity-60">Add Network</button>
+      </div>
+      <div>
+        <Link href="wallet"><a className="font-semibold text-8xl hover:opacity-60" >Get Started</a></Link>
+      </div>
+      <div>
+      </div>
+    </div>
+    <div className="fixed bottom-0 flex flex-row space-x-2">
+    
+    <svg className="hover:opacity-60" width="5%" role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+      <title>GitHub</title>
+      <a xlinkHref="https://www.github.com/lucaslokchan">
+      <path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"/>
+      </a>
+    </svg>
+
+    <svg className="hover:opacity-60" width="5%" role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+      <title>LinkedIn</title>
+      <a xlinkHref="https://www.linkedin.com/in/lucaslokchan/">
+      <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z"/>
+      </a>
+      </svg>
+    
+    <svg className="hover:opacity-60" width="5%" role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+      <title>Twitter</title>
+      <a xlinkHref="https://twitter.com/lucaslokchan">
+      <path d="M23.953 4.57a10 10 0 01-2.825.775 4.958 4.958 0 002.163-2.723c-.951.555-2.005.959-3.127 1.184a4.92 4.92 0 00-8.384 4.482C7.69 8.095 4.067 6.13 1.64 3.162a4.822 4.822 0 00-.666 2.475c0 1.71.87 3.213 2.188 4.096a4.904 4.904 0 01-2.228-.616v.06a4.923 4.923 0 003.946 4.827 4.996 4.996 0 01-2.212.085 4.936 4.936 0 004.604 3.417 9.867 9.867 0 01-6.102 2.105c-.39 0-.779-.023-1.17-.067a13.995 13.995 0 007.557 2.209c9.053 0 13.998-7.496 13.998-13.985 0-.21 0-.42-.015-.63A9.935 9.935 0 0024 4.59z"/>
+      </a>
+      </svg>
+    </div>
+  </div>  
+  </>
+  )
+}
