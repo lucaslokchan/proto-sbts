d32713c990ff73ce4d74f0557e4c617e86cb7459
lucaslokchan <lucas.lokchan@gmail.com>
2022-07-27 20:39:32 +0800

    Add responsiveness for mobile in wallet.js

--- pages/wallet.js
+++ pages/wallet.js
@@ -1,6 +1,8 @@
+import Head from "next/head";
 import { useState, useEffect } from "react";
 import FooterComponent from "./footer/footer";
 import sbtContract from "../sbt";
+import Router from "next/router";
 
 export default function Wallet() {
   const [isconnected, setIsConnected] = useState(false);
@@ -13,18 +15,17 @@ export default function Wallet() {
     if (typeof window.ethereum !== "undefined") {
       isConnectedHandler();
       addressChangeHandler();
-      onLoadHandler();
+      getTotalSupplyHandler().then((response) => setTotalSupply(response));
     }
-    getTotalSupplyHandler().then((response) => setTotalSupply(response));
   });
 
-  //useEffect(() => {
-  //  if (typeof window.ethereum !== "undefined") {
-  //    if (ethereum.selectedAddress != null) {
-  //      //getTokenOwnedHandler().then((response) => setTokenOwned(response));
-  //    }
-  //  }
-  //}, []);
+  useEffect(() => {
+    if (typeof window.ethereum !== "undefined") {
+      if (typeof ethereum.selectedAddress !== "undefined") {
+        onLoadHandler();
+      }
+    }
+  }, []);
 
   function truncateAddress(address) {
     let first = address.substr(0, 5);
@@ -103,12 +104,12 @@ export default function Wallet() {
     var tokenid = new Array();
     var urilist = new Array();
     var responselist = new Array();
+    setLoading(true);
     const balance = await sbtContract.methods
       .balanceOf(ethereum.selectedAddress)
       .call((err, result) => {
         return result;
-      })
-      .then(setLoading(true));
+      });
     for (let i = 0; i < balance; i++) {
       let id = await sbtContract.methods
         .tokenOfOwnerByIndex(ethereum.selectedAddress, i)
@@ -131,8 +132,12 @@ export default function Wallet() {
 
   return (
     <>
+      <Head>
+        <title>Soulbound - Wallet</title>
+        <meta name="description" content="Soulbound Token Wallet" />
+      </Head>
       <div className="border-8 border-red-800">
-        <div className="max-w-screen-xl mx-auto mt-24 border-2">
+        <div className="max-w-screen-xl mx-auto mt-2 md:mt-24 border-2">
           <div className="grid grid-cols-1 border-2 md+:grid-cols-2">
             <div className="mx-auto border-2 ">
               <div className="max-w-[450px] max-h-[280px] w-[340px] h-[220px] sm:w-[450px] sm:h-[280px] border-2 overflow-hidden border-black group rounded-2xl bg-white shadow-lg">
@@ -140,15 +145,42 @@ export default function Wallet() {
                   <h2 className="">Wallet Stats</h2>
                 </div>
                 {isconnected ? (
-                  <div className="grid grid-cols-2 my-10 border-2">
-                    <div className="border-2">
-                      <span>QR Code Here</span>
+                  <div className="grid grid-cols-2 mt-4 md:mt-10 border-2">
+                    <div className="border-2 mx-auto">
+                      <svg
+                        width="110"
+                        height="110"
+                        viewBox="0 0 110 110"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          d="M104.658 73.105V104.67H71.571V110H110V73.105H104.658Z"
+                          fill="#9F32B2"
+                        />
+                        <path
+                          d="M38.0822 104.67H5.34216V73.105H0V110H38.0822V104.67Z"
+                          fill="#9F32B2"
+                        />
+                        <path
+                          d="M5.34231 39.4352V5.32968H38.0824V0H0V39.4352H5.34231Z"
+                          fill="#9F32B2"
+                        />
+                        <path
+                          d="M71.571 5.32968H104.658V39.4352H110V0H71.571V5.32968Z"
+                          fill="#9F32B2"
+                        />
+                      </svg>
                     </div>
                     <div className="">
-                      <p>
-                        Address: {truncateAddress(ethereum.selectedAddress)}
-                      </p>
-                      <p>SBTs Owned: {tokenowned.length}</p>
+                      <div className="pt-2">
+                        <p>
+                          Address: {truncateAddress(ethereum.selectedAddress)}
+                        </p>
+                      </div>
+                      <div className="pt-2">
+                        <p>SBTs Owned: {tokenowned.length}</p>
+                      </div>
                     </div>
                   </div>
                 ) : (
@@ -161,7 +193,7 @@ export default function Wallet() {
               </div>
             </div>
             <div className="border-2">
-              <div className="border-2">
+              <div className="border-2 pt-2">
                 <div>
                   <h2 className="text-[#9F32B2]">Request SBTs</h2>
                 </div>
@@ -218,52 +250,184 @@ export default function Wallet() {
                   <p>SBTs Issued: {totalsupply}</p>
                 </div>
                 <div className="pt-4">
-                  <p>View on Block Explorer</p>
+                  <a href="https://ropsten.etherscan.io/address/0xAab2d8b6F6D3eE17510c87111e1563a4611FfFb2">
+                    <p>View on Block Explorer</p>
+                  </a>
                 </div>
               </div>
             </div>
           </div>
         </div>
 
-        <div className="max-w-screen-xl mx-auto mt-24 border-2">
-          <div className="flex justify-center">
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 md:gap-x-28 md:gap-y-16 gap-y-12 ">
-              {tokenowned.map((token) => {
-                return (
-                  <div>
-                    {[token].map((tokeninfo) => {
-                      return (
-                        <>
-                          <div className="flex">
-                            <div className="min-w-[300px] w-[300px] h-[350px] overflow-hidden border-2 border-black group rounded-2xl bg-white max-w-sm shadow-lg">
-                              <div className="py-7 bg-[#9F32B2]"></div>
-                              <div className="text-center mt-[5.5rem] mb-1">
-                                <p>{tokeninfo.title}</p>
-                              </div>
-                              <div className="mx-[2rem] border-t-[0.18rem] border-black">
-                                <div className="mt-2">
-                                  <span className="break-words">
-                                    {tokeninfo.description}
-                                  </span>
-                                </div>
-                              </div>
-                            </div>
-                            <div class="absloute">
-                              <img
-                                src={tokeninfo.image}
-                                class="shadow-xl rounded-full align-middle border-none border-black absolute -m-[-1rem] -ml-[13.5rem] max-w-[130px]"
-                              />
-                            </div>
-                          </div>
-                        </>
-                      );
-                    })}
-                  </div>
-                );
-              })}
+        {loading ? (
+          <div className="max-w-screen-xl mx-auto mt-2 md:mt-10 border-2">
+            <div className="">
+              <div className="text-center text-[#9F32B2]">
+                <h2>Loading SBTs</h2>
+              </div>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                style={{ margin: "auto" }}
+                width="80"
+                height="80"
+                display="block"
+                preserveAspectRatio="xMidYMid"
+                viewBox="0 0 100 100"
+              >
+                <circle cx="84" cy="50" r="10" fill="#9f32b2">
+                  <animate
+                    attributeName="r"
+                    begin="0s"
+                    calcMode="spline"
+                    dur="0.43859649122807015s"
+                    keySplines="0 0.5 0.5 1"
+                    keyTimes="0;1"
+                    repeatCount="indefinite"
+                    values="10;0"
+                  ></animate>
+                  <animate
+                    attributeName="fill"
+                    begin="0s"
+                    calcMode="discrete"
+                    dur="1.7543859649122806s"
+                    keyTimes="0;0.25;0.5;0.75;1"
+                    repeatCount="indefinite"
+                    values="#9f32b2;#9f32b2;#9f32b2;#9f32b2;#9f32b2"
+                  ></animate>
+                </circle>
+                <circle cx="16" cy="50" r="10" fill="#9f32b2">
+                  <animate
+                    attributeName="r"
+                    begin="0s"
+                    calcMode="spline"
+                    dur="1.7543859649122806s"
+                    keySplines="0 0.5 0.5 1;0 0.5 0.5 1;0 0.5 0.5 1;0 0.5 0.5 1"
+                    keyTimes="0;0.25;0.5;0.75;1"
+                    repeatCount="indefinite"
+                    values="0;0;10;10;10"
+                  ></animate>
+                  <animate
+                    attributeName="cx"
+                    begin="0s"
+                    calcMode="spline"
+                    dur="1.7543859649122806s"
+                    keySplines="0 0.5 0.5 1;0 0.5 0.5 1;0 0.5 0.5 1;0 0.5 0.5 1"
+                    keyTimes="0;0.25;0.5;0.75;1"
+                    repeatCount="indefinite"
+                    values="16;16;16;50;84"
+                  ></animate>
+                </circle>
+                <circle cx="50" cy="50" r="10" fill="#9f32b2">
+                  <animate
+                    attributeName="r"
+                    begin="-0.43859649122807015s"
+                    calcMode="spline"
+                    dur="1.7543859649122806s"
+                    keySplines="0 0.5 0.5 1;0 0.5 0.5 1;0 0.5 0.5 1;0 0.5 0.5 1"
+                    keyTimes="0;0.25;0.5;0.75;1"
+                    repeatCount="indefinite"
+                    values="0;0;10;10;10"
+                  ></animate>
+                  <animate
+                    attributeName="cx"
+                    begin="-0.43859649122807015s"
+                    calcMode="spline"
+                    dur="1.7543859649122806s"
+                    keySplines="0 0.5 0.5 1;0 0.5 0.5 1;0 0.5 0.5 1;0 0.5 0.5 1"
+                    keyTimes="0;0.25;0.5;0.75;1"
+                    repeatCount="indefinite"
+                    values="16;16;16;50;84"
+                  ></animate>
+                </circle>
+                <circle cx="84" cy="50" r="10" fill="#9f32b2">
+                  <animate
+                    attributeName="r"
+                    begin="-0.8771929824561403s"
+                    calcMode="spline"
+                    dur="1.7543859649122806s"
+                    keySplines="0 0.5 0.5 1;0 0.5 0.5 1;0 0.5 0.5 1;0 0.5 0.5 1"
+                    keyTimes="0;0.25;0.5;0.75;1"
+                    repeatCount="indefinite"
+                    values="0;0;10;10;10"
+                  ></animate>
+                  <animate
+                    attributeName="cx"
+                    begin="-0.8771929824561403s"
+                    calcMode="spline"
+                    dur="1.7543859649122806s"
+                    keySplines="0 0.5 0.5 1;0 0.5 0.5 1;0 0.5 0.5 1;0 0.5 0.5 1"
+                    keyTimes="0;0.25;0.5;0.75;1"
+                    repeatCount="indefinite"
+                    values="16;16;16;50;84"
+                  ></animate>
+                </circle>
+                <circle cx="16" cy="50" r="10" fill="#9f32b2">
+                  <animate
+                    attributeName="r"
+                    begin="-1.3157894736842104s"
+                    calcMode="spline"
+                    dur="1.7543859649122806s"
+                    keySplines="0 0.5 0.5 1;0 0.5 0.5 1;0 0.5 0.5 1;0 0.5 0.5 1"
+                    keyTimes="0;0.25;0.5;0.75;1"
+                    repeatCount="indefinite"
+                    values="0;0;10;10;10"
+                  ></animate>
+                  <animate
+                    attributeName="cx"
+                    begin="-1.3157894736842104s"
+                    calcMode="spline"
+                    dur="1.7543859649122806s"
+                    keySplines="0 0.5 0.5 1;0 0.5 0.5 1;0 0.5 0.5 1;0 0.5 0.5 1"
+                    keyTimes="0;0.25;0.5;0.75;1"
+                    repeatCount="indefinite"
+                    values="16;16;16;50;84"
+                  ></animate>
+                </circle>
+              </svg>
             </div>
           </div>
-        </div>
+        ) : (
+          <div className="max-w-screen-xl mx-auto mt-2 md:mt-24 border-2">
+            <div className="flex justify-center">
+              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 md:gap-28 gap-y-12">
+                {tokenowned.map((token) => {
+                  return (
+                    <div>
+                      {[token].map((tokeninfo) => {
+                        return (
+                          <>
+                            <div className="flex">
+                              <div className="min-w-[300px] w-[300px] h-[350px] overflow-hidden border-2 border-black group rounded-2xl bg-white max-w-sm shadow-lg">
+                                <div className="py-7 bg-[#9F32B2]"></div>
+                                <div className="text-center mt-[5.5rem] mb-1">
+                                  <p>{tokeninfo.title}</p>
+                                </div>
+                                <div className="mx-[2rem] border-t-[0.18rem] border-black">
+                                  <div className="mt-2">
+                                    <span className="break-words">
+                                      {tokeninfo.description}
+                                      {tokeninfo.type}
+                                    </span>
+                                  </div>
+                                </div>
+                              </div>
+                              <div class="absloute">
+                                <img
+                                  src={tokeninfo.image}
+                                  class="shadow-xl rounded-full align-middle border-none border-black absolute -m-[-1rem] -ml-[13.5rem] max-w-[130px]"
+                                />
+                              </div>
+                            </div>
+                          </>
+                        );
+                      })}
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          </div>
+        )}
         <FooterComponent></FooterComponent>
       </div>
     </>
